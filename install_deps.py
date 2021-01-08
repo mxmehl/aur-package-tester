48e29c5b7597701292218562723c0762f1b0126e
max.mehl <max.mehl@fsfe.org>
2021-01-08 13:36:51 +0100

    make more robust against missing dependencies

--- install_deps.py
+++ install_deps.py
@@ -10,18 +10,39 @@ def install(dependency):
     """Install a given package with yay"""
     subprocess.call(["yay", "--useask", "--noconfirm", "--needed", "-S", dependency])
 
+# Open file
 with open(f"{package}/.SRCINFO", "r") as file:
-    SRCINFO = file.read()
-    parsed = parse_srcinfo(SRCINFO)
+    parsed = parse_srcinfo(file.read())
+
+# Normal dependencies
+try:
     deps = parsed[0]["depends"]
+except KeyError:
+    deps = None
+    print("[INFO] no depends")
+
+if deps:
+    for dep in deps:
+        install(dep)
+
+# Make dependencies
+try:
     makedeps = parsed[0]["makedepends"]
+except KeyError:
+    makedeps = None
+    print("[INFO] no makedepends")
+
+if makedeps:
+    for dep in makedeps:
+        install(dep)
+
+# Check dependencies
+try:
     checkdeps = parsed[0]["checkdepends"]
+except KeyError:
+    checkdeps = None
+    print("[INFO] no checkdepends")
 
-for dep in deps:
-    install(dep)
-
-for dep in makedeps:
-    install(dep)
-
-for dep in checkdeps:
-    install(dep)
+if checkdeps:
+    for dep in checkdeps:
+        install(dep)
